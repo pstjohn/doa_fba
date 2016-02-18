1faedaea7a527f9ad75b2fa2cafaf7f3c7c4ad7c
Peter St. John <peterc.stjohn@gmail.com>
2016-02-18 09:38:34 -0700

    Rebasing EFMcollocation directly on base

--- EFMcollocation.py
+++ EFMcollocation.py
@@ -2,11 +2,12 @@ import pandas as pd
 import numpy as np
 
 import casadi as cs
+import cobra
 
 from .VariableHandler import VariableHandler
-from .DOAcollocation import DOAcollocation
+from .BaseCollocation import BaseCollocation
 
-class EFMcollocation(DOAcollocation):
+class EFMcollocation(BaseCollocation):
 
     def __init__(self, model, boundary_species, efms):
         """ A class to handle the dynamic optimization based method of dynamic
@@ -28,15 +29,36 @@ class EFMcollocation(DOAcollocation):
 
         """
 
-        super(EFMcollocation, self).__init__(model, boundary_species)
+        self.model = cobra.core.DataframeBasedModel(model)
+        # self.model.optimize(minimize_absolute_flux=1.0)
+
+        self.nx = len(boundary_species)
+        self.nv = efms.shape[0]
+        self.nm = len(self.model.metabolites)
+
+        # If tf is left as None, it will be a symbolic variable
+        self.tf = None
 
         assert self.nx == efms.shape[1], "EFMs are the wrong shape"
+        
+        # Handle boundary reactions
+        self.boundary_species = boundary_species
+        all_boundary_rxns = model.reactions.query('system_boundary', 'boundary')
+
+        self.boundary_rxns = []
+        for bs in boundary_species:
+            rxns = all_boundary_rxns.query(lambda r: r.reactants[0].id in bs)
+
+            assert len(rxns) == 1, (
+                "Error finding boundary reactions for {}: ".format(bs) + 
+                "{:d} reactions found".format(len(rxns)))
+
+            self.boundary_rxns += [rxns[0].id]
 
         # Assure that efms are in the correct order
         self.efms = efms.loc[:, self.boundary_rxns]
 
-        # Re-allocate for the EFM flux vector
-        self.nv = efms.shape[0]
+        super(EFMcollocation, self).__init__()
 
     def setup(self, mav=False):
         """ Set up the collocation framework """
@@ -47,7 +69,38 @@ class EFMcollocation(DOAcollocation):
         self._initialize_polynomial_constraints()
 
         self.col_vars['mav'] = mav
-        self.col_vars['kkt'] = False
+
+    def initialize(self, **kwargs):
+        """ Call after setting up boundary kinetics, finalizes the
+        initialization and sets up the NLP problem. Keyword arguments are
+        passed directly as options to the NLP solver """
+
+        if self.col_vars['mav']: self._initialize_mav_objective()
+        self._initialize_solver(**kwargs)
+
+    def plot_optimal(self):
+        """ Method to quickly plot an optimal solution. """
+
+
+        import matplotlib.pyplot as plt
+        import seaborn as sns
+        sns.set_style('darkgrid')
+        sns.set_context('talk')
+
+        fig, ax = plt.subplots(sharex=True, nrows=3, ncols=1, figsize=(12,10))
+
+        # Plot the results
+        lines = ax[0].plot(self.ts, self.sol[:,1:], '.--')
+        ax[0].legend(lines, self.boundary_species[1:], loc='upper left', ncol=2)
+    
+        # Plot the optimal fluxes
+        self._plot_optimal_fluxes(ax[1])
+
+        # Plot the biomass results
+        lines = ax[2].plot(self.ts, self.sol[:,0], '.--')
+        ax[2].legend(lines, self.boundary_species, loc='upper left', ncol=2)
+
+        plt.show()
 
 
     def add_boundary_constraints(self, constraint_dictionary):
@@ -93,13 +146,24 @@ class EFMcollocation(DOAcollocation):
                     self.col_vars['v_ub_sym'].loc[k, rxn] = ub
                     self.add_constraint(ub - rxn_sx, 0, cs.inf)
 
+    def _initialize_dynamic_model(self):
+        """ Initialize the model of biomass growth and substrate accumulation
+        """
 
+        t  = cs.SX.sym('t')          # not really used.
+        x  = cs.SX.sym('x', self.nx) # External metabolites
+        vb = cs.SX.sym('v', self.nx) # fluxes across the boundary
+
+        xdot = vb * x[0]             # Here we assume biomass is in x[0]
+
+        self.dxdt = cs.SXFunction('dxdt', [t,x,vb], [xdot])
 
     def _initialize_variables(self):
 
         core_variables = {
             'x'  : (self.nk, self.d+1, self.nx),
             'v'  : (self.nk, self.nv),
+            # 'a'  : (self.nk, self.d+1),
         }
 
         if self.tf == None: core_variables.update({'tf' : (1,)})
@@ -112,8 +176,13 @@ class EFMcollocation(DOAcollocation):
 
         # Initialize EFM bounds. EFMs are nonnegative.
         self.var.v_lb[:] = 0.
-        self.var.v_ub[:] = np.inf
+        self.var.v_ub[:] = cs.inf
         self.var.v_in[:] = 0.
+
+        # Activity polynomial.
+        # self.var.a_lb[:] = 0.
+        # self.var.a_ub[:] = np.inf
+        # self.var.a_in[:] = 1.
 
         if self.tf == None:
             self.var.tf_lb[:] = 1.
@@ -128,7 +197,6 @@ class EFMcollocation(DOAcollocation):
         self.col_vars['v_ub_sym'] = pd.DataFrame(
             np.empty((self.nk, self.nx), dtype=object),
             columns=self.boundary_rxns)
-
 
     def _initialize_polynomial_constraints(self):
         """ Add constraints to the model to account for system dynamics and
@@ -185,7 +253,33 @@ class EFMcollocation(DOAcollocation):
         self.x0 = {met : x_sx for met, x_sx in zip(self.boundary_species, x0)}
 
 
+    def _initialize_mav_objective(self):
+        """ Initialize the objective function to minimize the absolute value of
+        the flux vector """
 
+        self.objective_sx += (self.col_vars['alpha'] *
+                              cs.fabs(self.var.v_sx).values.sum())
+
+
+    def _plot_optimal_fluxes(self, ax):
+        active_fluxes = self.var.v_op.max(0) > 1E-4
+        ax.step(self.fs, self.var.v_op.loc[:, active_fluxes])
+
+    def _plot_setup(self):
+
+        # Create vectors from optimized time and states
+        if self.tf == None:
+            h = self.var.tf_op / self.nk
+        else: 
+            h = self.tf / self.nk
+
+        self.fs = h * np.arange(self.nk)
+        self.ts = np.array(
+            [point + h*np.array(self.col_vars['tau_root']) for point in 
+             np.linspace(0, h * self.nk, self.nk,
+                         endpoint=False)]).flatten()
+
+        self.sol = self.var.x_op.reshape((self.nk*(self.d+1)), self.nx)
         
         
         
